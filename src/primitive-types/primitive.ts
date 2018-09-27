6fd33be3760878a46bb351f1397a9c11932fa8ce
Stanislau Yudzitski <stanislauy@playtika.com>
2018-09-27 11:24:42 +0300

    Enum primitives

--- src/primitive-types/primitive.ts
+++ src/primitive-types/primitive.ts
@@ -1,3 +1,11 @@
-console.log('Hi there!');
+enum Amounts {
+  mon = 10,
+  tue = 14,
+  wed = 7,
+  thur = 21
+}
 
-let x:number;
+let a:number = Amounts.mon + Amounts.wed;
+let b:string|number = Amounts[21];
+console.log(a);
+console.log(b);
